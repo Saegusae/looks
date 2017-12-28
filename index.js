5124a98d0ab9b53f1bde570ef61e89e8801f6580
Saegusa <guzeltirpan.ege@gmail.com>
2017-12-28 09:38:47 +0300

    saving progress on copy

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ module.exports = function Looks(dispatch) {
   let command = Command(dispatch);
   
   let self, 
-    people = [];
+    people = [],
+    copy,
+    copyflag = false;
 
   dispatch.hook('S_LOGIN', 9, event => {
     self = event;
@@ -31,34 +33,63 @@ module.exports = function Looks(dispatch) {
     people = [];
   })
 
-  command.add('looks', name => {
-    if(name !== 'self') {
-      let data = people.filter(i => i.name.toLowerCase() === name.toLowerCase());
-      
-      if(!data || !(data.length > 0)) {
-        command.message(`(looks) Data for character ${name} was not found in your proximity.`);
-        return;
-      } else {
-        saveDataToJson(
-          formatCharacterData(
-            getCharacterData( data[0] )
-          )
-        );
-      }
-    } else {
-      saveDataToJson(
-        formatCharacterData(
-          getCharacterData( self )
-        )
-      );
-      // console.log( Uint8Array.from(self.details) );
+  command.add('looks', (...args) => {
+
+    if(args.length < 2) {
+      command.message('(looks) Malformed command.')
     }
+
+    switch(args[0]) {
+
+      case 'save':
+      case 'grab':
+      case 'json':
+        if(args[1] !== 'self') {
+          let data = people.filter(i => i.name.toLowerCase() === args[1].toLowerCase());
+          
+          if(!data || !(data.length > 0)) {
+            command.message(`(looks) Data for character ${args[1]} was not found in your proximity.`);
+            return;
+          } else {
+            saveDataToJson(
+              formatCharacterData(
+                getCharacterData( data[0] )
+              )
+            );
+          }
+        } else {
+          saveDataToJson(
+            formatCharacterData(
+              getCharacterData( self )
+            )
+          );
+        }
+        break;
+      case 'copy':
+
+        break;
+
+    }
+
+    
   });
 
   function getCharacterData(data) {
     let obj = {};
 
     obj['name'] = data.name;
+
+    obj['race'] = ['Human', 'High Elf', 'Aman', 'Castanic', 'Popori', 'Baraka', 'Elin'][
+      getRaceIndex(data.templateId)
+    ];
+    obj['gender'] = ['Male', 'Female'][
+      Math.floor(data.templateId / 100 % 2) + 1
+    ];
+    obj['class'] = ['Warrior', 'Lancer', 'Slayer', 'Berserker', 'Sorcerer',
+      'Archer', 'Mystic', 'Reaper', 'Gunner', 'Brawler', 'Ninja', 'Valkyrie'][
+        Math.floor((data.templateId - 100) / 200 % 50)
+      ];
+
     obj['details'] = Uint8Array.from( data.details );
     obj['shape'] = Uint8Array.from( data.shape );
 
@@ -69,6 +100,11 @@ module.exports = function Looks(dispatch) {
     let obj = {};
 
     obj['name'] = data.name;
+
+    obj['race'] = data.race;
+    obj['gender'] = data.gender;
+    obj['class'] = data['class'];
+
     obj['details'] = {};
     obj['shape'] = {};
     
@@ -103,8 +139,18 @@ module.exports = function Looks(dispatch) {
 
     fs.writeFile(`../node_modules/looks/data/${data.name}_${Date.now()}.json`, JSON.stringify(data, null, 2), (err) => {
       if(err) console.log(err);
-      else console.log('Character data successfully saved for ' + data.name);
+      else {
+        command.message(`Character data successfully saved for ${data.name}.`);
+        console.log('Character data successfully saved for ' + data.name);
+      }
     });
+  }
+
+  function getRaceIndex(template) {
+    if(Math.floor((template - 100) / 200 % 50) === 4 && 
+      (Math.floor(model / 100 % 2) + 1) === 1) return 6;
+    else
+      return Math.floor((template - 100) / 200 % 50);
   }
 
 }
